7b2a3429572af1a56da3724965f40ec97196f716
Viktor <viktor.bobryk@gmail.com>
2016-06-20 19:27:14 +0300

    fix mistake

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var bodyParser = require("body-parser");
-var indexPage = require('./indexPage.js');
+
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
-app.post('/', function (req, res) {
-   console.log(req.body);
-  res.send(indexPage.getPage(req.body));
-});
 app.get('/', function (req, res) {
   res.send("Hello World");
 });
